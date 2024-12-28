1ac305812501f21831fbab96355d860ceef90cb5
Ayan <ayanmullick@gmail.com>
2024-12-28 15:02:05 -0600

    .

--- One/run.ps1
+++ One/run.ps1
@@ -1,6 +1,7 @@
 using namespace System.Net
 param($Request, $TriggerMetadata)  # Input bindings are passed in via param block.
 
+Get-Module -ListAvailable
 Write-Host "PowerShell HTTP trigger function processed a request." # Write to the Azure Functions log stream.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{StatusCode = [HttpStatusCode]::OK  ; Body = "Hello $($request.Query.Name)"}) # Associate values to output bindings by calling 'Push-OutputBinding'.
 
