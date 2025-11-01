3c64a06c596e763fe77dfa52ab551a80224842a0
Ayan <ayanmullick@gmail.com>
2025-11-01 18:14:55 -0500

    Add content type.

--- One/run.ps1
+++ One/run.ps1
@@ -3,6 +3,13 @@ param($Request, $TriggerMetadata)  # Input bindings are passed in via param bloc
 
 Get-Module -ListAvailable
 Write-Host "PowerShell HTTP trigger function processed a request." # Write to the Azure Functions log stream.
-Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{StatusCode = [HttpStatusCode]::OK  ; Body = "Hello $($request.Query.Name)"}) # Associate values to output bindings by calling 'Push-OutputBinding'.
+$responsePayload = @{ message = "Hello $($request.Query.Name)" } | ConvertTo-Json -Depth 2
+Push-OutputBinding -Name Response -Value (
+	[HttpResponseContext]@{
+		StatusCode = [HttpStatusCode]::OK
+		Body       = $responsePayload
+		Headers    = @{ "Content-Type" = "application/json" }
+	}
+) # Associate values to output bindings by calling 'Push-OutputBinding'.
 
 #URL: https://ayanM.azurewebsites.net/api/One?name=Ayan
